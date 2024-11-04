b38f39964a8d25b6e8bb0aa0f9e03c14ea5b95a9
Arsalan Raza Azmi <arsalan.azmi92@gmail.com>
2024-11-05 00:22:09 +0500

    Work Data

---
+++ src/app/workData.tsx
@@ -0,0 +1,127 @@
+type Data = {
+  title: string;
+  description: string;
+  image?: string;
+  github: string;
+  url?: string;
+};
+export const data: Data[] = [
+  {
+    title: "Task Box",
+    description:
+      "Todo App with display filter options of all, completed & incomplete tasks",
+    image: "/images/Task-Box.png",
+    github: "https://github.com/arsalanazmi/Taskbox",
+    url: "https://bootcamp2020_taskbox.surge.sh/",
+  },
+  {
+    title: "React Todo List",
+    description: "React Todo List using Context APi & Local Storage.",
+    image: "/images/Todo-List.png",
+    github: "https://github.com/arsalanazmi/React-Todo-List",
+    url: "https://react-todo-list0.netlify.app/",
+  },
+  {
+    title: "Quiz App",
+    description: "A Quiz App with the user defined difficulty level & subject.",
+    image: "/images/Quiz-App2.png",
+    github: "https://github.com/arsalanazmi/Quiz-App",
+    url: "http://tricky_quiz.surge.sh/",
+  },
+  {
+    title: "React Quiz App",
+    description: "Quiz App with the progress bar.",
+    image: "/images/Quiz-App1.png",
+    github: "https://github.com/arsalanazmi/React-Quiz-App",
+    url: "https://quiz-app-progress.netlify.app/",
+  },
+  {
+    title: "Expense Tracker Progressive Web App",
+    description:
+      "A Pwa expense tracker app which can show your balance with the history of all the transactions.",
+    image: "/images/Expense-Tracker-App.png",
+    github: "https://github.com/arsalanazmi/Expense-Tracker-PWA-Web-App",
+    url: "https://expense-tracker-pwa-web-app.netlify.app/",
+  },
+  {
+    title: "Shopping Basket",
+    description:
+      "An Ecommerce shoe website (Only Frontend. No payment method).",
+    image: "/images/Nike-Shoes.png",
+    github: "https://github.com/arsalanazmi/Shopping-Basket",
+    url: "https://nike-shoe-shopping-basket.netlify.app/",
+  },
+  {
+    title: "Multi Step Form",
+    description: "React Multi Step Form using Formik & Yup.",
+    image: "/images/Multi-Step-Form.png",
+    github: "https://github.com/arsalanazmi/Multi-Step-Form",
+    url: "https://multiplestep-form.netlify.app/",
+  },
+  {
+    title: "Online Shopping Store",
+    description: "An online shopping store web template.",
+    image: "/images/Online-Shop.png",
+    github: "https://github.com/arsalanazmi/Online-Shopping",
+    url: "https://online-shoppping.netlify.app/",
+  },
+  {
+    title: "Data Finance",
+    description: "A simple data website template using React & Tailwind CSS.",
+    image: "/images/Data-Finance.png",
+    github: "https://github.com/arsalanazmi/Data-Finance",
+    url: "https://data-finance-react-app.netlify.app/",
+  },
+  
+  {
+    title: "Tiny Short Web Animations",
+    description:
+      "A single web page with the text coming in the animated fashion.",
+    image: "/images/Tiny-Short-Web-Animations.png",
+    github: "https://github.com/arsalanazmi/Tiny-Short-Web-Animations",
+    url: "http://tiny_short_web_animations.surge.sh/",
+  },
+  {
+    title: "Web-Animations",
+    description: "An animated scene whose speed increases on mouse click.",
+    image: "/images/Animation-Scene.png",
+    github: "https://github.com/arsalanazmi/Web-Animations",
+    url: "https://web_animations.surge.sh/",
+  },
+  {
+    title: "Restaurant Template",
+    description: "A sample restaurent web template with some animations.",
+    image: "/images/Restaurent-Template.png",
+    github: "https://github.com/arsalanazmi/Restaurent-Template",
+    url: "https://eatery-cafe.netlify.app/",
+  },
+  {
+    title: "Password Generator App",
+    description: "Unique password generator app with variable characters.",
+    image: "/images/Password-Generator-App.png",
+    github: "https://github.com/arsalanazmi/Password-Generator-App",
+    url: "https://unique-password-generator01.netlify.app/",
+  },
+  {
+    title: "CountDown Timer",
+    description: "A simple countdown timer app.",
+    image: "/images/Timer-App.png",
+    github: "https://github.com/arsalanazmi/Timer-App",
+    url: "https://timer-application.netlify.app/",
+  },
+  {
+    title: "Number Guess Game",
+    description: "A simple number guessing game between user & computer.",
+    image: "/images/Guess-Number.png",
+    github: "https://github.com/arsalanazmi/Number-Guess",
+    url: "https://number-guessing-game001.netlify.app/",
+  },
+  {
+    title: "BMI Calculator",
+    description:
+      "A simple BMI calculator that calculates BMI based on your height in cm & weight in kg.",
+    image: "/images/BMI-Calculator.png",
+    github: "https://github.com/arsalanazmi/BMI-Calculator",
+    url: "https://bmicalculator09.netlify.app/",
+  },
+];
